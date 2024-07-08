bf576eadc1b77e5ea1e23fbd72db3e6944f01966
kedar-ayare <kedarayareilr@gmail.com>
2024-07-08 16:37:40 +0530

    change2

--- index.php
+++ index.php
@@ -1,33 +1,34 @@
 <?php
+    echo "hello there"
   // Get the new query from the POST request
     // Set the file path and name
-    $file_path = "queries.json";
+    // $file_path = "queries.json";
 
-    //   echo "Kedar";
+    // //   echo "Kedar";
 
-    //   echo $_GET['name'];
+    // //   echo $_GET['name'];
 
-    // Get the contents of the JSON file
-    $file_contents = file_get_contents($file_path);
+    // // Get the contents of the JSON file
+    // $file_contents = file_get_contents($file_path);
 
-    // Decode the JSON contents into a PHP array
-    $data = json_decode($file_contents, true);
+    // // Decode the JSON contents into a PHP array
+    // $data = json_decode($file_contents, true);
 
-    // Get the query data from the HTTP POST request
-    $query_data = array(
-        "name" => $_GET["name"],
-        "email" => $_GET["email"],
-        "subject" => $_GET["subject"]
-        "query" => $_GET["query"],
-        "timestamp" => time()
-    );
+    // // Get the query data from the HTTP POST request
+    // $query_data = array(
+    //     "name" => $_GET["name"],
+    //     "email" => $_GET["email"],
+    //     "subject" => $_GET["subject"]
+    //     "query" => $_GET["query"],
+    //     "timestamp" => time()
+    // );
 
-    // Add the new query data to the top of the array
-    array_unshift($data, $query_data);
+    // // Add the new query data to the top of the array
+    // array_unshift($data, $query_data);
 
-    // Encode the updated data back into JSON
-    $new_file_contents = json_encode($data);
+    // // Encode the updated data back into JSON
+    // $new_file_contents = json_encode($data);
 
-    // Save the updated JSON data back into the file
-    file_put_contents($file_path, $new_file_contents);
+    // // Save the updated JSON data back into the file
+    // file_put_contents($file_path, $new_file_contents);
 ?>
