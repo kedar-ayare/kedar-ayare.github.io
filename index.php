25cd8749ade26b470901699eb26a8d67dd859cc6
kedar-ayare <kedarayareilr@gmail.com>
2025-01-31 20:09:01 +0530

    New Responsive code

--- index.php
+++ index.php
@@ -1,35 +1,38 @@
 <?php
-    echo "hello there"
+    echo "hello there";
   // Get the new query from the POST request
     // Set the file path and name
-    // $file_path = "queries.json";
+    $file_path = "queries.json";
 
     echo "Kedar";
 
     echo $_GET['name'];
-    echo $_GET['subject']
+    echo $_GET['email'];
+    echo $_GET['subject'];
+    echo $_GET['query'];
 
-    // // Get the contents of the JSON file
-    // $file_contents = file_get_contents($file_path);
 
-    // // Decode the JSON contents into a PHP array
-    // $data = json_decode($file_contents, true);
+    // Get the contents of the JSON file
+    $file_contents = file_get_contents($file_path);
 
-    // // Get the query data from the HTTP POST request
-    // $query_data = array(
-    //     "name" => $_GET["name"],
-    //     "email" => $_GET["email"],
-    //     "subject" => $_GET["subject"]
-    //     "query" => $_GET["query"],
-    //     "timestamp" => time()
-    // );
+    // Decode the JSON contents into a PHP array
+    $data = json_decode($file_contents, true);
 
-    // // Add the new query data to the top of the array
-    // array_unshift($data, $query_data);
+    // Get the query data from the HTTP POST request
+    $query_data = array(
+        "name" => $_GET["name"],
+        "email" => $_GET["email"],
+        "subject" => $_GET["subject"],
+        "query" => $_GET["query"],
+        "timestamp" => time()
+    );
 
-    // // Encode the updated data back into JSON
-    // $new_file_contents = json_encode($data);
+    // Add the new query data to the top of the array
+    array_unshift($data, $query_data);
 
-    // // Save the updated JSON data back into the file
-    // file_put_contents($file_path, $new_file_contents);
+    // Encode the updated data back into JSON
+    $new_file_contents = json_encode($data);
+
+    // Save the updated JSON data back into the file
+    file_put_contents($file_path, $new_file_contents);
 ?>
